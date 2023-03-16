c2834c2c6b5eca14af719e0463c399c66e4a412f
Kristan Ruíz <itan_07@hotmail.com>
2023-03-15 18:37:54 -0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+namespace EjercicioGraficoLogo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.CreateGraphics().Clear(Color.White);
+
+            Graphics papel;
+            papel = pictureBox1.CreateGraphics();
+            Pen lapiz = new Pen(Color.Blue);
+
+            papel.DrawRectangle(lapiz, 10, 20, 60, 60);
+            papel.DrawRectangle(lapiz, 10, 20, 40, 40);
+            papel.DrawRectangle(lapiz, 10, 20, 20, 20);
+
+            papel.DrawRectangle(lapiz, 100, 100, 60, 60);
+            papel.DrawRectangle(lapiz, 100, 100, 40, 40);
+            papel.DrawRectangle(lapiz, 100, 100, 20, 20);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.CreateGraphics().Clear(Color.White);
+
+            Graphics papel;
+            papel = pictureBox1.CreateGraphics();
+            Pen lapiz = new Pen(Color.Blue);
+
+            papel.DrawLine(lapiz, 20, 80, 70, 10);
+            papel.DrawLine(lapiz, 70, 10, 120, 80);
+            papel.DrawLine(lapiz, 20, 80, 120, 80);
+        }
+    }
+}
