f11e2be90e7e6d3d155b3839f29e943b12c6ded5
Kristan Ruíz <itan_07@hotmail.com>
2023-03-19 20:24:58 -0800

    Agregando créditos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -76,11 +77,21 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hecho por: Kristan Ruíz Limón y Diana Yulissa Sesma Santiago";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 281);
+            this.ClientSize = new System.Drawing.Size(560, 305);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
@@ -91,6 +102,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -100,5 +112,6 @@
         private PictureBox pictureBox1;
         private Button button2;
         private Button button3;
+        private Label label1;
     }
 }
